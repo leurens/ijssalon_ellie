5af18e29636535bd152e80bfc60c6925348b1c45
Test NHA <laurens.pouders@hotmail.com>
2024-02-10 14:22:58 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,19 +1,25 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
+from helper import decoreer
 
-aanbieding = prijzen["vanille"] * 0.8
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
 
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    aanbieding = prijzen["vanille"] * 0.8
 
-reclame_tekst3 = reclame_tekst.upper()
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
 
-reclame_tekst4 = reclame_tekst3.split()
+    reclame_tekst3 = reclame_tekst.upper()
 
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el.upper())
-    else:
-        print(el.lower())
+    reclame_tekst4 = reclame_tekst3.split()
+
+    for el in reclame_tekst4:
+        if len(el) >= 5:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
